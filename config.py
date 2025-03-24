fc5864db0d7a30aa228a3376529b879b00f633cd
Sagnik Chakraborty <sagnikgraviton847@gmail.com>
2025-03-24 12:07:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class Config:
     API_HASH = os.environ.get("API_HASH", "fbe5152195458e6aa8ec4298c96fb274")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "7545122233:AAGlDCqGFJJRL75T8UYXERUQlMwzAgkBb3A") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
-    DB_URL = os.environ.get("DB_URL", "mongodb+srv://sagnikgraviton847:AutoForward@autoforward.iwsux.mongodb.net/?retryWrites=true&w=majority&appName=AutoForward")
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://sagnikgraviton847:AutoForward@autoforward.iwsux.mongodb.net/?retryWrites=true&w=majority&appName=AutoForward/AutoForward")
     DB_NAME = os.environ.get("DB_NAME", "AutoForward")
     OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '1226915127').split()]
 
