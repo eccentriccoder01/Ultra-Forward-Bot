6ea324e7a9ad4c8454ccdbc4fc3c77788be80007
Sagnik Chakraborty <sagnikgraviton847@gmail.com>
2025-03-24 11:30:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,13 +9,13 @@
 import os
 
 class Config:
-    API_ID = os.environ.get("API_ID", "")
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+    API_ID = os.environ.get("API_ID", "23294809")
+    API_HASH = os.environ.get("API_HASH", "fbe5152195458e6aa8ec4298c96fb274")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7545122233:AAGlDCqGFJJRL75T8UYXERUQlMwzAgkBb3A") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
-    DB_URL = os.environ.get("DB_URL", "")
-    DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
-    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '').split()]
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://sagnikgraviton847:AutoForward@autoforward.iwsux.mongodb.net/?retryWrites=true&w=majority&appName=AutoForward")
+    DB_NAME = os.environ.get("DB_NAME", "AutoForward")
+    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '1226915127').split()]
 
 
 class temp(object): 
